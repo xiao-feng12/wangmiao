6d85d8a48b4c52181b3413d7e2c2d171d40a7e3a
db <iori-free@163.com>
2017-12-23 10:51:35 +0800

    创建首个web页面

--- src/main/java/com/wangmiao/demo/controller/FirstController.java
+++ src/main/java/com/wangmiao/demo/controller/FirstController.java
@@ -1,13 +1,31 @@
 package com.wangmiao.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class FirstController {
+    //
+    //    @RequestMapping("/")
+    //    String index(){
+    //        return "Hello wangmiao";
+    //    }
+
+    @Value("${userName}")
+    private String userName;
+
+    @Value("${bookTitle}")
+    private String bookTitle;
 
     @RequestMapping("/")
-    String index(){
-        return "Hello wangmiao";
+    public String index(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("name", userName);
+        map.addAttribute("bookTitle", bookTitle);
+        // return模板文件的名称，对应src/main/resources/templates/welcome.html
+        return "welcome";
     }
+
 }
