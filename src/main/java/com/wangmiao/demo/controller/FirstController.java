6c890445c5ee3142ce20ab7ce71f226d0d6afdcb
db <iori-free@163.com>
2017-12-24 17:10:16 +0800

    替换为 thymeleaf

--- src/main/java/com/wangmiao/demo/controller/FirstController.java
+++ src/main/java/com/wangmiao/demo/controller/FirstController.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class FirstController {
 
-    @Value("${userName}")
-    private String userName;
+    @Value("${names}")
+    private String names;
 
     @Value("${bookTitle}")
     private String bookTitle;
@@ -21,19 +21,10 @@ public class FirstController {
     private UserRepository userRepository;
 
     @RequestMapping("/")
-    public String index(ModelMap map) {
-        // 加入一个属性，用来在模板中读取
-        map.addAttribute("name", userName);
-        map.addAttribute("bookTitle", bookTitle);
-        // return模板文件的名称，对应src/main/resources/templates/welcome.html
-        User user = userRepository.getOne(1L);
-        map.addAttribute("userId",user.getuId());
-        map.addAttribute("userName",user.getName());
-        return "welcome";
-    }
-
-    @RequestMapping("/thymeleaf")
     public String thymeleaf(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("names", names);
+        map.addAttribute("bookTitle", bookTitle);
         User user = userRepository.getOne(1L);
         map.addAttribute("name",user.getName());
         return "hello";
