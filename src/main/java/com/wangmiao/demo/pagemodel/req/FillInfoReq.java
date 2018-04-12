4dc462f9cddfe527f4cbd955894b97a513f2829b
db <db@umu360.com>
2018-04-12 11:26:08 +0800

    分页模板，待做成Demo

--- src/main/java/com/wangmiao/demo/pagemodel/req/FillInfoReq.java
+++ src/main/java/com/wangmiao/demo/pagemodel/req/FillInfoReq.java
@@ -10,4 +10,60 @@ public class FillInfoReq {
     private int age;
     private String enducationDegree;
 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhonenumber() {
+        return phonenumber;
+    }
+
+    public void setPhonenumber(int phonenumber) {
+        this.phonenumber = phonenumber;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEnducationDegree() {
+        return enducationDegree;
+    }
+
+    public void setEnducationDegree(String enducationDegree) {
+        this.enducationDegree = enducationDegree;
+    }
 }
